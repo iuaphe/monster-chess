c3ecb269392b496fc41271944084ae10b4ec0ce1
acou12 <51276518+acou12@users.noreply.github.com>
2023-07-03 12:49:34 -0400

    Update `docs`

--- docs/_app/immutable/chunks/1-645ac368.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-0e6fba56.js";export{t as component};
--- docs/_app/immutable/chunks/2-43b2ed40.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-b7b18044.js";export{t as component};
